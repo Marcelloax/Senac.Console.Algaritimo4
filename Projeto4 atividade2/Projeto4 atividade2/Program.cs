9ade28610647d3243e38aeb643caae1010787153
Marcelloax <marcellomachado2014@gmail.com>
2025-04-01 21:43:40 -0300

    URI1001

---
+++ Projeto4 atividade2/Projeto4 atividade2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int X = A + B;
+Console.WriteLine("x="+X);
