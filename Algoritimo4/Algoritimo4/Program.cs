4485d6ac1df99e76ac8e961a67a64b330d3642a8
Marcelloax <marcellomachado2014@gmail.com>
2025-04-01 21:19:10 -0300

    Projeto4

--- Algoritimo4/Algoritimo4/Program.cs
+++ Algoritimo4/Algoritimo4/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Globalization;
+
+//Variaveis
+double largura;
+double comprimento, area, valormetro, valorterreno;
+
+//Entrada
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valormetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//Calculo
+area = largura * comprimento;
+valorterreno = area * valormetro;
+
+//Saida
+Console.WriteLine("AREA - " + area.ToString("F2",CultureInfo.InvariantCulture));
+Console.WriteLine("PRECO = " + valorterreno.ToString("F2", CultureInfo.InvariantCulture));
